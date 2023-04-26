f8f6eedce522215f72186e3dce44f1c057d0ad77
Van Zhu <van@highsoft.ltd>
2023-04-26 21:42:13 +0800

    feat: frontends

---
+++ frontends/monolithic.webui/jest.config.mjs
@@ -0,0 +1,26 @@
+import {quasarEsModulesPackageNames} from '@quasar/quasar-app-extension-testing-unit-jest/jest-preset.mjs';
+
+/** @type {import('jest').Config} */
+export default {
+  preset: '@quasar/quasar-app-extension-testing-unit-jest',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    },
+  },
+  transform: {
+    [`^(${quasarEsModulesPackageNames}).+\\.js$`]: 'babel-jest',
+    '^.+\\.(ts|js|html)$': [
+      'ts-jest',
+      {
+        // Remove if using `const enums`
+        // See https://kulshekhar.github.io/ts-jest/docs/getting-started/options/isolatedModules/
+        isolatedModules: true,
+      },
+    ],
+  },
+};
