79b1ddd41951e23cfc1b62e730a38c17195337cd
Neil Wang <neil@scrumaster.org>
2023-05-04 13:11:39 +0800

    refactor: make Credential record

--- foundations/iam.domain/src/main/java/ltd/highsoft/hare/foundations/iam/domain/Credential.java
+++ foundations/iam.domain/src/main/java/ltd/highsoft/hare/foundations/iam/domain/Credential.java
@@ -3,50 +3,10 @@ package ltd.highsoft.hare.foundations.iam.domain;
 import ltd.highsoft.hare.frameworks.domain.core.Id;
 import ltd.highsoft.hare.frameworks.domain.core.PasswordEncoder;
 
-public class Credential {
-
-    private final Id id;
-    private final CredentialOwner owner;
-    private final CredentialType type;
-    private final LoginName loginName;
-    private final EncryptedSecret secret;
-    private final boolean predefined;
-
-    public Credential(Id id, CredentialOwner owner, CredentialType type, LoginName loginName, EncryptedSecret secret, boolean predefined) {
-        this.id = id;
-        this.type = type;
-        this.loginName = loginName;
-        this.secret = secret;
-        this.owner = owner;
-        this.predefined = predefined;
-    }
+public record Credential(Id id, CredentialOwner owner, CredentialType type, LoginName loginName, EncryptedSecret secret, boolean predefined) {
 
     public MatchResult matchSecret(PlanSecret plan, PasswordEncoder passwordEncoder) {
         return secret.match(plan, passwordEncoder) ? MatchResult.success(this) : MatchResult.fail("iam.username-or-password-wrong");
-    }
-
-    public Id id() {
-        return this.id;
-    }
-
-    public CredentialOwner owner() {
-        return this.owner;
-    }
-
-    public CredentialType type() {
-        return type;
-    }
-
-    public LoginName loginName() {
-        return this.loginName;
-    }
-
-    public EncryptedSecret secret() {
-        return this.secret;
-    }
-
-    public boolean predefined() {
-        return predefined;
     }
 
 }
