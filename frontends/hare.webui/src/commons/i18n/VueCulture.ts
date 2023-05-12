50880222d84611dc0f93673a93d4feae65cd925c
Van Zhu <van@highsoft.ltd>
2023-05-13 00:45:35 +0800

    refactor: VueCulture.ts

--- frontends/hare.webui/src/commons/i18n/VueCulture.ts
+++ frontends/hare.webui/src/commons/i18n/VueCulture.ts
@@ -3,10 +3,10 @@ import { createI18n, I18n } from 'vue-i18n';
 import { App } from "vue";
 
 export class VueCulture {
-  private static _instance: I18n<unknown, unknown, unknown, false>;
+  private static instance: I18n<unknown, unknown, unknown, false>;
 
   public static initialize(app: App): void {
-    VueCulture._instance = createI18n({
+    VueCulture.instance = createI18n({
       legacy: false,
       globalInjection: true,
       locale: 'zh-CN',
@@ -17,9 +17,5 @@ export class VueCulture {
 
   public static translate(code: string, ...args: unknown[]): string {
     return (VueCulture.instance.global as { t: (code: string, ...args: unknown[]) => string }).t(code, args);
-  }
-
-  public static get instance(): I18n<unknown, unknown, unknown, false> {
-    return this._instance;
   }
 }
