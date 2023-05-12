8f9249218f28d5dee973558880abfdab5d50f05c
Van Zhu <van@highsoft.ltd>
2023-05-12 10:02:41 +0800

    refactor: rename ApplicationTabBarActions to PageHostActionsControl

--- frontends/hare.webui/src/layouts/ApplicationHeaderControl.test.ts
+++ frontends/hare.webui/src/layouts/ApplicationHeaderControl.test.ts
@@ -1,18 +1,18 @@
-import {describe, expect, test} from '@jest/globals';
-import {mount} from '@vue/test-utils';
-import ApplicationHeaderBar from 'layouts/ApplicationHeaderBar.vue';
-import {Application} from 'layouts/Application';
-import {reactive} from 'vue';
+import { describe, expect, test } from '@jest/globals';
+import { mount } from '@vue/test-utils';
+import { Application } from 'layouts/Application';
+import { reactive } from 'vue';
+import ApplicationHeaderControl from "layouts/ApplicationHeaderControl.vue";
 import StartMenu from 'layouts/StartMenu.vue';
 import ApplicationTitle from 'layouts/ApplicationTitle.vue';
 import ProfileMenuButton from 'layouts/ProfileMenuButton.vue';
-import {setupComponentTest} from 'app/test/utils/component';
+import { setupComponentTest } from 'app/test/utils/component';
 
 setupComponentTest();
 
-describe('ApplicationHeaderBar', () => {
+describe('ApplicationHeaderControl', () => {
   test('should present users the main-menu button, the application-title and the user-profile-menu button', () => {
-    const wrapper = mount(ApplicationHeaderBar, {props: {model: reactive(new Application())}});
+    const wrapper = mount(ApplicationHeaderControl, { props: { model: reactive(new Application()) } });
     expect(wrapper.findComponent(StartMenu)).toBeDefined();
     expect(wrapper.findComponent(ApplicationTitle).text()).toBe('瀚诚企业应用框架');
     expect(wrapper.findComponent(ProfileMenuButton)).toBeDefined();
--- frontends/hare.webui/src/layouts/MainLayout.test.ts
+++ frontends/hare.webui/src/layouts/MainLayout.test.ts
@@ -1,19 +1,19 @@
-import {describe, expect, test} from '@jest/globals';
-import {mount} from '@vue/test-utils';
-import {setupComponentTest} from 'app/test/utils/component';
+import { describe, expect, test } from '@jest/globals';
+import { mount } from '@vue/test-utils';
+import { setupComponentTest } from 'app/test/utils/component';
 import MainLayout from 'layouts/MainLayout.vue';
-import ApplicationHeaderBar from 'layouts/ApplicationHeaderBar.vue';
-import {createApplicationRouter} from 'src/router';
-import ApplicationTabBar from 'layouts/ApplicationTabBar.vue';
-import {RouterView} from 'vue-router';
+import { createApplicationRouter } from 'src/router';
+import { RouterView } from 'vue-router';
+import ApplicationHeaderControl from "layouts/ApplicationHeaderControl.vue";
+import PageHostControl from "layouts/PageHostControl.vue";
 
 setupComponentTest();
 
 describe('MainLayout', () => {
   test('should present users the application frame', () => {
-    const wrapper = mount(MainLayout, {global: {plugins: [createApplicationRouter()]}});
-    expect(wrapper.findComponent(ApplicationHeaderBar)).toBeDefined();
-    expect(wrapper.findComponent(ApplicationTabBar)).toBeDefined();
+    const wrapper = mount(MainLayout, { global: { plugins: [createApplicationRouter()] } });
+    expect(wrapper.findComponent(ApplicationHeaderControl)).toBeDefined();
+    expect(wrapper.findComponent(PageHostControl)).toBeDefined();
     expect(wrapper.findComponent(RouterView)).toBeDefined();
   });
 });
