27264cc2b3840532e7dfbb4c19c1383a85f309e7
Van Zhu <van@highsoft.ltd>
2023-05-17 00:55:33 +0800

    feat: VueCulture should be able to localize messages by underlying vue-i18n

---
+++ frontends/hare.webui/src/commons/i18n/VueCulture.test.ts
@@ -0,0 +1,23 @@
+import {beforeEach, describe, expect, it, jest} from '@jest/globals';
+import {createI18n, I18n} from 'vue-i18n';
+import {mockDeep} from 'jest-mock-extended';
+import {VueCulture} from 'commons/i18n/VueCulture';
+import {App} from 'vue';
+import {MockInstance} from 'jest-mock';
+
+jest.mock('vue-i18n');
+
+describe('VueCulture', () => {
+  const mockCreateI18n = createI18n as jest.MockedFunction<typeof createI18n>;
+  const mockI18n = mockDeep<I18n<unknown, unknown, unknown, unknown, false>>();
+
+  beforeEach(() => {
+    mockCreateI18n.mockReturnValue(mockI18n);
+  });
+
+  it('should be able to localize messages by underlying vue-i18n', () => {
+    const culture = new VueCulture(mockDeep<App>() as App);
+    (mockI18n.global.t as unknown as MockInstance).mockReturnValue('translated-message');
+    expect(culture.localize('message.test', 'abc')).toBe('translated-message');
+  });
+});
