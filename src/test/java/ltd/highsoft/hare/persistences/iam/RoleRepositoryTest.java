9ce4ac47a66064ca8b5689179f26e13698993ea9
Raymond <raymond-liao@outlook.com>
2023-05-17 09:14:45 +0800

    test: #76 should_create_role_succeed

--- src/test/java/ltd/highsoft/hare/persistences/iam/RoleRepositoryTest.java
+++ src/test/java/ltd/highsoft/hare/persistences/iam/RoleRepositoryTest.java
@@ -1,15 +1,23 @@
 package ltd.highsoft.hare.persistences.iam;
 
 import ltd.highsoft.hare.PostgresTest;
+import ltd.highsoft.hare.foundations.iam.domain.Role;
 import ltd.highsoft.hare.foundations.iam.domain.Roles;
 import ltd.highsoft.hare.foundations.iam.gateways.persistence.DbRoleRepository;
 import ltd.highsoft.hare.foundations.iam.gateways.persistence.RoleMapper;
 import ltd.highsoft.hare.frameworks.domain.core.AggregateNotFoundException;
+import ltd.highsoft.hare.frameworks.domain.core.Code;
 import ltd.highsoft.hare.frameworks.domain.core.Id;
 import ltd.highsoft.hare.frameworks.domain.core.ScopedId;
+import ltd.highsoft.hare.frameworks.security.core.GrantedAuthorities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static ltd.highsoft.hare.frameworks.domain.core.Name.name;
+import static ltd.highsoft.hare.frameworks.domain.core.Remarks.remarks;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class RoleRepositoryTest extends PostgresTest {
     private Roles.RoleRepository roleRepository;
@@ -20,12 +28,27 @@ public class RoleRepositoryTest extends PostgresTest {
     }
 
     @Test
+    public void should_create_role_succeed() {
+        Role role = new Role(ScopedId.id("role-a", "highsoft"), name("Role A"), GrantedAuthorities.of("f1"), remarks("Role A remarks"), false, Code.code("1"));
+        roleRepository.save(role);
+        Role role1 = roleRepository.get(Id.id("role-a"));
+        assertThat(role1.id()).isEqualTo(role.id());
+        assertThat(role1.code()).isEqualTo(role.code());
+        assertThat(role1.name()).isEqualTo(role.name());
+        assertThat(role1.grantedAuthorities()).isEqualTo(role.grantedAuthorities());
+        assertThat(role1.remarks()).isEqualTo(role.remarks());
+        assertThat(role1.predefined()).isEqualTo(role.predefined());
+    }
+
+    @Test
     public void should_throw_error_when_role_not_exists_id() {
-        Assertions.assertThrowsExactly(AggregateNotFoundException.class, () -> roleRepository.get(Id.id("role-1")), "error.role-not-found");
+        Assertions.assertThrowsExactly(AggregateNotFoundException.class, () -> roleRepository.get(Id.id("role-1")),
+                "error.role-not-found");
     }
 
     @Test
     public void should_throw_error_when_role_not_exists_scoped_id() {
-        Assertions.assertThrowsExactly(AggregateNotFoundException.class, () -> roleRepository.get(ScopedId.id("role-1", "highsoft")), "error.role-not-found");
+        Assertions.assertThrowsExactly(AggregateNotFoundException.class, () -> roleRepository.get(ScopedId.id("role-1", "highsoft")),
+                "error.role-not-found");
     }
 }
