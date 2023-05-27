7c090bcf89aaef02fc68b00da489a09f3e2d8f20
Van Zhu <van@highsoft.ltd>
2023-05-27 15:12:07 +0800

    refactor

--- frontends/hare.webui/src/commons/security/GrantedAuthorities.test.ts
+++ frontends/hare.webui/src/commons/security/GrantedAuthorities.test.ts
@@ -5,10 +5,12 @@ describe('GrantedAuthorities', () => {
   it('should be authenticated when created in normal', () => {
     expect(grantedAuthorities('user.add').authorities.has('authenticated')).toBeTruthy();
     expect(grantedAuthorities('user.add').authenticated).toBeTruthy();
+    expect(grantedAuthorities('user.add').anonymous).toBeFalsy();
   });
 
   it('should be anonymous when created as anonymous', () => {
     expect(GrantedAuthorities.ANONYMOUS.authorities.has('anonymous')).toBeTruthy();
     expect(GrantedAuthorities.ANONYMOUS.anonymous).toBeTruthy();
+    expect(GrantedAuthorities.ANONYMOUS.authenticated).toBeFalsy();
   });
 });
