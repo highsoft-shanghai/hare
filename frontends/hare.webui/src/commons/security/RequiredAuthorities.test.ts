f479c798d5d5098d1866741b651122015972ee33
Van Zhu <van@highsoft.ltd>
2023-05-27 14:55:48 +0800

    feat: RequiredAuthorities should mismatch granted authorities when no same authority found in both sides

--- frontends/hare.webui/src/commons/security/RequiredAuthorities.test.ts
+++ frontends/hare.webui/src/commons/security/RequiredAuthorities.test.ts
@@ -3,7 +3,14 @@ import {requiredAuthorities} from 'commons/security/RequiredAuthorities';
 import {grantedAuthorities} from 'commons/security/GrantedAuthorities';
 
 describe('RequiredAuthorities', () => {
-  it('should match granted authorities which contains any authority in both sides', () => {
+  it('should match granted authorities when same authority found in both sides', () => {
     expect(requiredAuthorities('user.add').matches(grantedAuthorities('user.add'))).toBeTruthy();
+    expect(requiredAuthorities('user.update').matches(grantedAuthorities('user.search', 'user.update'))).toBeTruthy();
+    expect(requiredAuthorities('user.update', 'project.update').matches(grantedAuthorities('user.search', 'user.update'))).toBeTruthy();
+  });
+
+  it('should mismatch granted authorities when no same authority found in both sides', () => {
+    expect(requiredAuthorities('user.add').matches(grantedAuthorities('user.update'))).toBeFalsy();
+    expect(requiredAuthorities('user.add', 'user.update').matches(grantedAuthorities('user.search', 'user.delete'))).toBeFalsy();
   });
 });
