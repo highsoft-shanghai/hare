77cd68ad6ced3f7435fcd29a998831fd8d72dda8
Van Zhu <van@highsoft.ltd>
2023-05-27 17:05:17 +0800

    feat: AuthorizationResult should be able to represent forbidden authorization

--- frontends/hare.webui/src/commons/security/AuthorizationResult.test.ts
+++ frontends/hare.webui/src/commons/security/AuthorizationResult.test.ts
@@ -1,10 +1,16 @@
 import {describe, expect, it} from '@jest/globals';
-import {success} from 'commons/security/AuthorizationResult';
+import {forbidden, success} from 'commons/security/AuthorizationResult';
 
 describe('AuthorizationResult', () => {
   it('should be able to represent successful authorization', () => {
     const authorizationResult = success();
     expect(authorizationResult.approved).toBeTruthy();
     expect(authorizationResult.redirect).toBeUndefined();
+  });
+
+  it('should be able to represent forbidden authorization', () => {
+    const authorizationResult = forbidden();
+    expect(authorizationResult.approved).toBeFalsy();
+    expect(authorizationResult.redirect).toBeFalsy();
   });
 });
