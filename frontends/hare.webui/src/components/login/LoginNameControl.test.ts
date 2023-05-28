efc1a4ef5d9e576f668f84d638625d06b5576b1a
Van Zhu <van@highsoft.ltd>
2023-05-28 16:02:57 +0800

    feat: LoginNameControl should be able present values from model

--- frontends/hare.webui/src/components/login/LoginNameControl.test.ts
+++ frontends/hare.webui/src/components/login/LoginNameControl.test.ts
@@ -1,17 +1,30 @@
-import {describe, expect, test} from '@jest/globals';
-import {mount} from '@vue/test-utils';
+import {beforeEach, describe, expect, it} from '@jest/globals';
+import {flushPromises, mount} from '@vue/test-utils';
 import {setupComponentTest} from 'app/test/utils/component';
 import {LoginNameModel} from 'components/login/LoginNameModel';
 import LoginNameControl from 'components/login/LoginNameControl.vue';
-import {reactive} from 'vue';
+import {ref} from 'vue';
 
 setupComponentTest();
 
 describe('LoginNameControl', () => {
-  test('should present users the an input box, a label and an icon', () => {
-    const wrapper = mount(LoginNameControl, {props: {model: reactive(new LoginNameModel())}});
+  const model = ref(new LoginNameModel());
+
+  beforeEach(() => {
+    model.value = new LoginNameModel();
+  });
+
+  it('should present users the an input box, a label and an icon', () => {
+    const wrapper = mount(LoginNameControl, {props: {model: model.value}});
     expect(wrapper.find('input').exists()).toBeTruthy();
     expect(wrapper.find('.q-field__label').text()).toBe('用户名');
     expect(wrapper.find('.q-icon').text()).toBe('perm_identity');
+  });
+
+  it('should be able present values from model', async () => {
+    const wrapper = mount(LoginNameControl, {props: {model: model.value}});
+    model.value.handleInput('abc');
+    await flushPromises();
+    expect(wrapper.find('input').element.value).toBe('abc');
   });
 });
