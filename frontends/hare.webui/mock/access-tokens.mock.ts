31e22b408ccf53e4a3f916321d39035d82430311
Van Zhu <van@highsoft.ltd>
2023-05-29 01:49:55 +0800

    refactor: mock apis

--- frontends/hare.webui/mock/access-tokens.mock.ts
+++ frontends/hare.webui/mock/access-tokens.mock.ts
@@ -1,11 +1,21 @@
 import {defineMock} from 'vite-plugin-mock-dev-server';
 
 // noinspection JSUnusedGlobalSymbols
-export default defineMock({
-  url: '/api/access-tokens/access-token.mock.id',
-  delay: 500,
-  body: {
-    id: 'access-token.mock.id',
-    grantedAuthorities: []
+export default defineMock([
+  {
+    url: '/api/access-tokens/access-token.mock.admin',
+    delay: 500,
+    body: {
+      id: 'access-token.mock.admin',
+      grantedAuthorities: ['admin']
+    }
+  },
+  {
+    url: '/api/access-tokens/access-token.mock.user',
+    delay: 500,
+    body: {
+      id: 'access-token.mock.user',
+      grantedAuthorities: []
+    }
   }
-});
+]);
