cc80867e4e50a57285027d5c1197bb9d160f908e
Van Zhu <van@highsoft.ltd>
2023-05-28 16:50:22 +0800

    refactor: use TextInputControl where possible

--- frontends/hare.webui/src/components/login/LoginNameControl.vue
+++ frontends/hare.webui/src/components/login/LoginNameControl.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-input :label="model.label" hint="" :hide-hint="false" :model-value="model.value" @update:model-value="e => model.handleInput(e)">
+  <text-input-control :model="model">
     <template #prepend>
       <q-icon name="perm_identity"/>
     </template>
-  </q-input>
+  </text-input-control>
 </template>
+
 <script setup lang="ts">
 import {UnwrapRef} from 'vue';
 import {TextInputModel} from 'components/base/TextInputModel';
+import TextInputControl from 'components/base/TextInputControl.vue';
 
 defineProps<{
   model: UnwrapRef<TextInputModel>
