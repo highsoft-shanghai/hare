95bdba9b7b70827788d9c497181b47faf246434e
Van Zhu <van@highsoft.ltd>
2023-05-28 17:00:32 +0800

    refactor

--- frontends/hare.webui/src/components/base/TextInputControl.test.ts
+++ frontends/hare.webui/src/components/base/TextInputControl.test.ts
@@ -2,34 +2,34 @@ import {beforeEach, describe, expect, it} from '@jest/globals';
 import {flushPromises, mount} from '@vue/test-utils';
 import {setupComponentTest} from 'app/test/utils/component';
 import {TextInputModel} from 'components/base/TextInputModel';
-import {ref} from 'vue';
+import {reactive, UnwrapRef} from 'vue';
 import {i18n} from 'commons/i18n/i18n';
 import TextInputControl from 'components/base/TextInputControl.vue';
 
 setupComponentTest();
 
 describe('TextInputControl', () => {
-  const model = ref(new TextInputModel(i18n('label.login-name')));
+  let model: UnwrapRef<TextInputModel>;
 
   beforeEach(() => {
-    model.value = new TextInputModel(i18n('label.login-name'));
+    model = reactive(new TextInputModel(i18n('label.login-name')));
   });
 
   it('should be able to present values from model', async () => {
-    const wrapper = mount(TextInputControl, {props: {model: model.value}});
-    model.value.handleInput('abc');
+    const wrapper = mount(TextInputControl, {props: {model: model}});
+    model.handleInput('abc');
     await flushPromises();
     expect(wrapper.find('input').element.value).toBe('abc');
   });
 
   it('should update model when user input texts', async () => {
-    const wrapper = mount(TextInputControl, {props: {model: model.value}});
+    const wrapper = mount(TextInputControl, {props: {model: model}});
     await wrapper.find('input').setValue('input text');
-    expect(model.value.value).toBe('input text');
+    expect(model.value).toBe('input text');
   });
 
   it('should be able to support prepend slot', async () => {
-    const wrapper = mount(TextInputControl, {props: {model: model.value}, slots: {prepend: 'Prepend Content'}});
+    const wrapper = mount(TextInputControl, {props: {model: model}, slots: {prepend: 'Prepend Content'}});
     expect(wrapper.find('.q-field__prepend').text()).toBe('Prepend Content');
   });
 });
