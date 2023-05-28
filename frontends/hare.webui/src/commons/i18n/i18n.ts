70dbe306aab81f41d96f77f7fb422cec5206a323
Van Zhu <van@highsoft.ltd>
2023-05-28 17:05:47 +0800

    feat: TextInputControl should present users a text input and a label

--- frontends/hare.webui/src/commons/i18n/i18n.ts
+++ frontends/hare.webui/src/commons/i18n/i18n.ts
@@ -1,6 +1,11 @@
 import {I18nMessage} from 'commons/i18n/I18nMessage';
 import {Message} from 'commons/i18n/Message';
+import {SimpleMessage} from 'commons/i18n/SimpleMessage';
 
 export function i18n(code: string, ...args: unknown[]): Message {
   return new I18nMessage(code, args);
+}
+
+export function message(value: string): Message {
+  return new SimpleMessage(value);
 }
--- frontends/hare.webui/src/components/base/TextInputControl.test.ts
+++ frontends/hare.webui/src/components/base/TextInputControl.test.ts
@@ -3,7 +3,7 @@ import {flushPromises, mount} from '@vue/test-utils';
 import {setupComponentTest} from 'app/test/utils/component';
 import {TextInputModel} from 'components/base/TextInputModel';
 import {reactive, UnwrapRef} from 'vue';
-import {i18n} from 'commons/i18n/i18n';
+import {message} from 'commons/i18n/i18n';
 import TextInputControl from 'components/base/TextInputControl.vue';
 
 setupComponentTest();
@@ -12,7 +12,13 @@ describe('TextInputControl', () => {
   let model: UnwrapRef<TextInputModel>;
 
   beforeEach(() => {
-    model = reactive(new TextInputModel(i18n('label.login-name')));
+    model = reactive(new TextInputModel(message('Label Content')));
+  });
+
+  it('should present users a text input and a label', async () => {
+    const wrapper = mount(TextInputControl, {props: {model: model}});
+    expect(wrapper.find('input').exists()).toBeTruthy();
+    expect(wrapper.find('.q-field__label').text()).toBe('Label Content');
   });
 
   it('should be able to present values from model', async () => {
