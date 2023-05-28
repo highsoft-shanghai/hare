a5142943f3c6e2d78cec0bc44cc49091a7926506
Van Zhu <van@highsoft.ltd>
2023-05-28 16:40:29 +0800

    feat: TextInputControl

---
+++ frontends/hare.webui/src/components/base/TextInputControl.test.ts
@@ -0,0 +1,30 @@
+import {beforeEach, describe, expect, it} from '@jest/globals';
+import {flushPromises, mount} from '@vue/test-utils';
+import {setupComponentTest} from 'app/test/utils/component';
+import {TextInputModel} from 'components/base/TextInputModel';
+import {ref} from 'vue';
+import {i18n} from 'commons/i18n/i18n';
+import TextInputControl from 'components/base/TextInputControl.vue';
+
+setupComponentTest();
+
+describe('TextInputControl', () => {
+  const model = ref(new TextInputModel(i18n('label.login-name')));
+
+  beforeEach(() => {
+    model.value = new TextInputModel(i18n('label.login-name'));
+  });
+
+  it('should be able to present values from model', async () => {
+    const wrapper = mount(TextInputControl, {props: {model: model.value}});
+    model.value.handleInput('abc');
+    await flushPromises();
+    expect(wrapper.find('input').element.value).toBe('abc');
+  });
+
+  it('should update model when user input texts', async () => {
+    const wrapper = mount(TextInputControl, {props: {model: model.value}});
+    await wrapper.find('input').setValue('input text');
+    expect(model.value.value).toBe('input text');
+  });
+});
---
+++ frontends/hare.webui/src/components/base/TextInputControl.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-input :label="model.label" hint="" :hide-hint="false" :model-value="model.value" @update:model-value="e => model.handleInput(e)">
+  </q-input>
+</template>
+<script setup lang="ts">
+import {UnwrapRef} from 'vue';
+import {TextInputModel} from 'components/base/TextInputModel';
+
+defineProps<{
+  model: UnwrapRef<TextInputModel>
+}>();
+</script>
