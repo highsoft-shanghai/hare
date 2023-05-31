f3281236b49125d2b7340722692d522ece4d023a
Van Zhu <van@highsoft.ltd>
2023-06-01 00:45:07 +0800

    feat: Password should hash underlying text when serialize to json

--- frontends/hare.webui/src/components/login/Password.test.ts
+++ frontends/hare.webui/src/components/login/Password.test.ts
@@ -10,4 +10,9 @@ describe('Password', () => {
     password.assign('new value');
     expect(password).toEqual(new Password('new value'));
   });
+
+  it('should hash underlying text when serialize to json', () => {
+    const password = new Password('123456');
+    expect(password.toJSON()).toBe('ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413');
+  });
 });
